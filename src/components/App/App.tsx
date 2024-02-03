193e1929cfc80fd4739d6bffaf77c5fb21c34f91
diego callegari <callcallegari@gmail.com>
2024-02-03 18:01:59 +0100

    Create Layout component and add to main

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,12 @@
+import Layout from "../Layout/Layout";
+
 const App = (): React.ReactElement => {
-  return <p>Hello world!</p>;
+  return (
+    <>
+      <p>Hello world!</p>
+      <Layout />
+    </>
+  );
 };
 
 export default App;
