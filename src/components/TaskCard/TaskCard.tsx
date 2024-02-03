d85ac8adeac6bf67b45ad16d2ca36e75ce7cffcb
diego callegari <callcallegari@gmail.com>
2024-02-03 18:07:18 +0100

    Add TaskCard and style

---
+++ src/components/TaskCard/TaskCard.tsx
@@ -0,0 +1,11 @@
+import TaskCardStyled from "./TaskCardStyle";
+
+const TaskCard = (): React.ReactElement => {
+  return (
+    <TaskCardStyled>
+      <h1>Hola card!</h1>
+    </TaskCardStyled>
+  );
+};
+
+export default TaskCard;
